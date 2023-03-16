95919c4f589f97a6e10756b086e8a0bf5b61e332
Lamin Jawla <laminjawla1001@gmail.com>
2023-03-16 08:21:26 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints a mesage with the printf functiion
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}

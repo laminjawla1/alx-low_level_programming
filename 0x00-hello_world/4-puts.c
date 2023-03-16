903a989900f303d2c3df35c5eaf4bd3d7a9e206f
Lamin Jawla <laminjawla1001@gmail.com>
2023-03-16 08:09:07 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
-
 /**
  * Prints "Programming is like building a multilingual puzz" and exit 
- */
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

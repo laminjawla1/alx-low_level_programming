cdba1f22fc35ebe1cf530aecfda23a625b02de87
Lamin Jawla <laminjawla1001@gmail.com>
2023-03-17 14:52:33 +0000

    Hexadecmal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,13 +7,14 @@
 int main(void)
 {
 	int c;
+	char ch;
 
 	for (c = 48; c < 58; c++)
 	{
 		putchar(c);
 		if (c == 57)
 		{
-			for (char ch = 'a'; ch <= 'f'; ch++)
+			for (ch = 'a'; ch <= 'f'; ch++)
 			{
 				putchar(ch);
 			}

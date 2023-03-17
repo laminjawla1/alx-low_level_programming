513f8c3f6de8c965865c2e1ebb190e81769a23a8
Lamin Jawla <laminjawla1001@gmail.com>
2023-03-17 13:10:17 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - print numbers 0 - 9
+*using ascii notation
+*Return: 0
+*/
+
+int main(void)
+{
+	int c;
+	
+	for (c = 48; c < 58; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}

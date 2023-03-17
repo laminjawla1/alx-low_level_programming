9bc20ee0bd50130e2fad4adf340c9e08ffbe8de8
Lamin Jawla <laminjawla1001@gmail.com>
2023-03-17 12:19:30 +0000

    Lower case alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+*main - prints the letters of the alphabet
+*in lower case
+*Return: 0 (success)
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+}

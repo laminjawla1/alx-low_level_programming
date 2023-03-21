af5d2dea3d996c36f023a4f1892107ecf330d3b5
Lamin Jawla <laminjawla1001@gmail.com>
2023-03-21 11:27:12 +0000

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*main - call the print_alphabet function
+*that prints the alphabets in lowercse 10x
+*Return: On success 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		print_alphabet_x10();
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - prints the alphabet
+*in lower case
+*Return: void
+*/
+
+void print_alphabet_x10(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
+

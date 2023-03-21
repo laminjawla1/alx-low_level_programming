290b8fe5ddc89ede11e37382502af6f812838fe1
Lamin Jawla <laminjawla1001@gmail.com>
2023-03-21 18:56:06 +0000

    last digit

---
+++ 0x02-functions_nested_loops/7-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*main - test the code
+*Return: 0
+*/
+
+int main(void)
+{
+	int r;
+
+	r = print_last_digit(98);
+	_putchar('0' + r);
+	_putchar('\n');
+
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*print_last_digit - get the last digit of num
+*@n: the to get its last digit
+*Return: the last digit of a num
+*/
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}

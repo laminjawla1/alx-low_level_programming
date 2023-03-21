a5334a4a08aa19674df516258ebaedaaadd6b470
Lamin Jawla <laminjawla1001@gmail.com>
2023-03-21 10:08:15 +0000

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "_putchar.h"
+#include <string.h>
+
+/**
+*main - print the contents of the text
+*variable a character after other
+*using the Holberton School
+*custom  _putchar function
+* Return: On success 0.
+*/
+
+int main(void)
+{
+	size_t i;
+	char *text;
+
+	text = "_putchar";
+	for (i = 0; i < strlen(text); i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+	return (0);
+}

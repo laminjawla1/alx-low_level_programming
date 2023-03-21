7b17b6e8c06dacfbdce39b0da282364ce1fe1b92
Lamin Jawla <laminjawla1001@gmail.com>
2023-03-21 18:46:31 +0000

    There is no such things as absolute value in this world. You can only estimate what a this is worth to you

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,21 +1,6 @@
 #include "main.h"
 
 /**
-*_get_sqrt - compute the sqrt of a num
-*@n: the number to be calculated
-*Return: the sqrt of a num
-*/
-
-int _get_sqrt(int n)
-{
-	float sqrt;
-
-	for (sqrt = 0.01; sqrt * sqrt < n; sqrt += 0.01);
-	return ((int) sqrt);
- 
-}
-
-/**
 *_abs - get the absolute value of a number
 *@n: the num to get the abs
 *Return: the abs of a number
@@ -23,5 +8,9 @@ int _get_sqrt(int n)
 
 int _abs(int n)
 {
-	return (_get_sqrt(n * n));
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	return (n);
 }

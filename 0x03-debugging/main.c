7ca3dbee152e35a364f477aa897b7e2f5777dc02
Lamin Jawla <laminjawla1001@gmail.com>
2023-03-22 16:16:54 +0000

    Multiple mains

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -4,9 +4,11 @@
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
-
 int main(void)
 {
-	printf("%d\n", largest_number(3, 1, 2));
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
 	return (0);
 }

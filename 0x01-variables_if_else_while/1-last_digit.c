8ceaa5d30beaaf61661befe9caf9cfd177d19a96
Lamin Jawla <laminjawla1001@gmail.com>
2023-03-22 06:44:34 -0700

    Merge

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,24 +6,26 @@
  * Return: 0 (success)
 */
 int main(void)
-{
-	int n, last_digit_of_n;
+
+	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-    last_digit_of_n = n % 10;
+	int last_digit_of_n = n % 10;
 
-    if (last_digit_of_n > 5)
-        printf(
-                "Last digit of %d is %d and is greater than %d\n", 
+    	if (last_digit_of_n > 5)
+	{
+        	printf("Last digit of %d is %d and is greater than %d\n", 
                 n, last_digit_of_n, 5);
-    else if (last_digit_of_n < 6 && last_digit_of_n != 0)
-        printf(
-                "Last digit of %d is %d and is less than %d and not %d\n", 
+	}
+    	else if (last_digit_of_n < 6 && last_digit_of_n != 0)
+	{
+       		printf("Last digit of %d is %d and is less than %d and not %d\n", 
                 n, last_digit_of_n, 6, 0);
+	}
 	else
-        printf(
-                "Last digit of %d is %d and is %d\n", n, last_digit_of_n, 0);
-
-    return (0);
+	{
+        	printf("Last digit of %d is %d and is %d\n", n, last_digit_of_n, 0);
+	}
+	return (0);
 }

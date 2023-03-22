0f065b19f27d0ccf19121900bee3fe36ebae022b
Lamin Jawla <laminjawla1001@gmail.com>
2023-03-22 13:16:07 +0000

    Multiple mains

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Generate a random number and checks its parity
+ * Return: 0 (Success)
+ */
+
+void positive_or_negative(int n)
+{
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
+
+	return (0);
+}

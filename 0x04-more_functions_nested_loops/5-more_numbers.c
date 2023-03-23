e77fd895165c8e3b4d23cfdf706478c3423614de
Lamin Jawla <laminjawla1001@gmail.com>
2023-03-23 09:19:44 +0000

    Added Readme

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*more_numbers - print  numbers from 0 to 14
+*ten time
+*Return: nothing
+*/
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			_putchar((j % 10) + '0');
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}

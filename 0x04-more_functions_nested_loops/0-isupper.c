ce00416bd6a53d5f3cfe9c27220eead822355acd
Lamin Jawla <laminjawla1001@gmail.com>
2023-03-23 08:09:36 +0000

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*_isupper - checks if a character is upper
+*@c: a character to be checked
+*Return: 1 or 0
+*/
+
+int _isupper(int c)
+{
+	return ((c >= 'A' && c <= 'Z'));
+}

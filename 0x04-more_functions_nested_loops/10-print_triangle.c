3eca593561315a285bc87ca2e3f664efe200ea24
Lamin Jawla <laminjawla1001@gmail.com>
2023-03-23 11:48:18 +0000

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*print_triangle - prints a triangle
+*@size: height of the triangle
+*Return: void
+*/
+
+void print_triangle(int size)
+{
+	int i, j, s;
+
+	for (i = 0; i < size; i++)
+	{
+		for (s = 0; s < (size - i - 1); s++)
+		{
+			_putchar(' ');
+		}
+		for (j = 0; j <= i; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void print_triangle(int size);
 void print_square(int size);
 int _isupper(int c);
 int _isdigit(int c);

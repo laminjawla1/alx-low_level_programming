8d1de66c8e4e6f128df1e449a263715fb486497a
Lamin Jawla <laminjawla1001@gmail.com>
2023-03-27 16:43:54 +0000

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,64 @@
+#include <ctype.h>
+#include <stdio.h>
+
+int calc(char *s)
+{
+	int n;
+
+	for (n = 0; *s; s++)
+		if (isdigit(*s))
+			n = 10 * n + (*s - '0');
+	return (n);
+}
+int _atoi(char *s)
+{
+	int n, sign;
+
+	while (isspace(*s))
+		s++;	/* get rid of white spaces */
+
+	if (isdigit(*s))
+	{
+		return (calc(s));
+	}
+	if (*s == '+' || *s == '-')
+	{
+		if (isdigit(*(s + 1)))
+		{
+			sign = (*s == '-') ? -1 : 1;
+			return (calc(s) * sign);
+		}
+		else
+		{
+			while (!isdigit(*s))
+				s++;
+			for (n = 0; isdigit(*s); s++)
+				n = 10 * n + (*s - '0');
+			return (n);
+		}
+	}
+	if (*s == '+' || *s == '-')
+	{
+		while (!isdigit(*s))
+			s++;
+		if (*(s - 1) == '+' || *(s - 1) == '-')
+		{
+			sign = (*(s - 1) == '-') ? -1 : 1;
+			for (n = 0; *s; s++)
+				if (isdigit(*s))
+					n = 10 * n + (*s - '0');
+				else
+					return (n);
+			return (n * sign);
+		}
+	}
+	else
+	{
+		while (!isdigit(*s))
+			s++;
+		for (n = 0; isdigit(*s); s++)
+			n = 10 * n + (*s - '0');
+		return (n);
+	}
+	return (0);
+}

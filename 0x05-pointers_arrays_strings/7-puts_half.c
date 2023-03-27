13abda34ec5ac14258f217e46a952e35d923bb67
Lamin Jawla <laminjawla1001@gmail.com>
2023-03-27 14:28:25 +0000

    Print half of a str

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,7 +11,7 @@ void puts_half(char *str)
 	int i, len;
 
 	len = strlen(str);
-	for (i = (len - 1) / 2; i < len; i++)
+	for (i = (len - 2) / 2; i < len; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 }

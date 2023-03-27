ecd13e2dd63cb851fe2e973be98dfb76cabf0895
Lamin Jawla <laminjawla1001@gmail.com>
2023-03-27 12:28:36 +0000

    Reverse a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,7 +9,7 @@ void rev_string(char *s)
 	int i, j, len, t;
 
 	len = strlen(s) - 1;
-	for (i = 0, j = len; i < j / 2; i++)
+	for (i = 0, j = len; i <= j / 2; i++)
 	{
 		t = s[i];
 		s[i] = s[j - i];

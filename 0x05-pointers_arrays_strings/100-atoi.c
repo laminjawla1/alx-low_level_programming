5e93143a73c2fecf394e363bfa1e67e1e7fabd5e
Lamin Jawla <laminjawla1001@gmail.com>
2023-03-27 17:31:05 +0000

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -23,14 +23,14 @@ int _atoi(char *s)
 		if (s[i] == '-')
 			++d;
 
-		if (s[i] >= '0' && s[i] <= '9')
+		if (isdigit(s[i]))
 		{
 			digit = s[i] - '0';
 			if (d % 2)
 				digit = -digit;
 			n = n * 10 + digit;
 			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
+			if (!isdigit(s[i + 1]))
 				break;
 			f = 0;
 		}

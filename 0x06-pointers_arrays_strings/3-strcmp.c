c43193d027b23880fc5dfaa27e508c36ef245b09
Lamin Jawla <laminjawla1001@gmail.com>
2023-03-29 11:24:23 +0000

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include <string.h>
+/**
+*_strcmp - compares s1 to s2
+*@s1: first string param
+*@s2: Second string param
+*Return: 0 if s1 == s1, -15 if s1 < s1, 15 if s1 > s2
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int i, n, cnt;
+
+	n = strlen(s1);
+	cnt = 0;
+	for (i = 0; i < n; i++)
+		if (s1[i] == s2[i])
+			cnt++;
+	if (cnt == n)
+		return (0);
+	else if (s1[0] < s2[0])
+		return (-15);
+	else
+		return (15);
+}

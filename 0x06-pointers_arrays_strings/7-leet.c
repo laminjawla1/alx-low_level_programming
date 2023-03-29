c1abbbf003858cd19fe138648568e26a1f8493b9
Lamin Jawla <laminjawla1001@gmail.com>
2023-03-29 16:45:39 +0000

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,15 +7,15 @@ char *leet(char *s)
 {
 	int i, j, n;
 	char letters[] = {'a', 'e', 'o', 't', 'l'};
-	int numbers[] = {4, 3, 0, 7, 1};
+	char codes[] = {'4', '3', '0', '7', '1'};
 
 	n = sizeof(letters) / sizeof(letters[0]);
 	i = 0;
 	while (s[i])
 	{
 		for (j = 0; j < n; j++)
-			if (s[i] == letters[j] || s[i] - 32 == letters[j])
-				s[i] = numbers[j] + '0';
+			if (s[i] == letters[j] || s[i]  == letters[j] - 32)
+				s[i] = codes[j];
 		i++;
 	}
 	return (s);

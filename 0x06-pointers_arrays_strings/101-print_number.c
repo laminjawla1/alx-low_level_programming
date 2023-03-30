94bae116db2dcd449dc8efabad153537a96b6ae0
Lamin Jawla <laminjawla1001@gmail.com>
2023-03-30 08:47:55 +0000

    Print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,12 +7,14 @@
 */
 void print_number(int n)
 {
+	unsigned int tmp;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		tmp = -n;
 	}
-	if (n / 10)
-		print_number(n / 10);
-	_putchar(n % 10 + '0');
+	if (tmp / 10)
+		print_number(tmp / 10);
+	_putchar(tmp % 10 + '0');
 }

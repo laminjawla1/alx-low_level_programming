aed480622a06709034e797cd25933819374cb0d7
Lamin Jawla <laminjawla1001@gmail.com>
2023-04-01 08:11:02 +0000

    main.h

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Generate a random number and checks its parity
+ * Return: 0 (Success)
+ */
+void positive_or_negative(int n)
+{
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+}

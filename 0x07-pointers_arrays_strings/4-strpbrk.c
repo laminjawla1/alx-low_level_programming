f2e527407e3d1be7f450a92e52c58a1c68fa5db4
Lamin Jawla <laminjawla1001@gmail.com>
2023-04-03 17:48:36 +0000

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,38 +1,18 @@
-#include <stdio.h>
 /**
-*in - checks if a char is in a str
-*@s: string to be searched
-*@c: character to be find
-*Return: 1 or 0
-*/
-int in(char *s, char c)
-{
-	int i, cnt = 0;
-
-	for (i = 0; s[i]; i++)
-		if (s[i] == c)
-			cnt++;
-	return (cnt);
-}
-/**
-*_strspn -  gets the length of a prefix substring
+*_strpbrk -  gets the length of a prefix substring
 *@s: The string
 *@accept: The substring
-*Return: Num of chars in accept and s
+*Return: Pointer to first occurence of s in a
 */
 char *_strpbrk(char *s, char *accept)
 {
-	unsigned int cnt = 0;
-	int i, n;
-	char *p = 0;
+	int i, j;
 
 	for (i = 0; s[i]; i++)
 	{
-		n = in(accept, s[i]);
-		if (n)
-			p[k] = s[i];
-		else
-			return (p);
+		for (j = 0; accept[j]; j++)
+			if (s[i] == accept[j])
+				return (s + i);
 	}
-	return (p);
+	return (0);
 }

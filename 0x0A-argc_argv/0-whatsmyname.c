247eff14a444dceb47a2a1df011e1f86f29b6688
Lamin Jawla <laminjawla1001@gmail.com>
2023-04-08 09:15:46 +0000

    my name is

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,8 +5,8 @@
 *@argv: Argument Vector
 *Return: (0)
 */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", argv[argc - 1]);
+	printf("%s\n", *argv);
 	return (0);
 }

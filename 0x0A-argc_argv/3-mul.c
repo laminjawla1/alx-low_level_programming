da85323a8e51dca82978e17587e146020a01a1c1
Lamin Jawla <laminjawla1001@gmail.com>
2023-04-08 10:44:58 +0000

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+*get_int - converts a string to an integer
+*@s: Str Param
+*Return: number
+*/
+int get_int(char *s)
+{
+	int i, n = 0;
+
+	for (i = 0; s[i]; i++)
+		n = 10 * n + (s[i] - '0');
+	return (n);
+}
+/**
+*main - multiplies all the numbers passed as arguments
+*and prints them
+*@argc: Argument Count
+*@argv: Argument vector
+*Return: (0)
+*/
+int main(int argc, char *argv[])
+{
+	int n1, n2;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	n1 = get_int(argv[1]);
+	n2 = get_int(argv[2]);
+
+	printf("%d\n", n1 + n2);
+	return (0);
+}

c87b9eb4d6c7cccc3edf6b8de00217b757803944
Lamin Jawla <laminjawla1001@gmail.com>
2023-04-11 10:24:07 +0000

    strcat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+
+/**
+*get_len - gets the length of a string
+*@s: string
+*Return: len of the string in int
+*/
+int get_len(char *s)
+{
+	int i = 0, len = 0;
+
+	while (s[i])
+	{
+		len++;
+		i++;
+	}
+	return (len);
+}
+/**
+*str_concat - concatenates two strings
+*@s1: first param
+*@s2: second param
+*Return: pointer to the concatenated result
+*/
+char *str_concat(char *s1, char *s2)
+{
+	char *result;
+	int len1, len2, i, j;
+
+	len1 = get_len(s1);
+	len2 = get_len(s2);
+
+	result = (char *)malloc(sizeof(char) * (len1 + len2) + 1);
+
+	for (i = 0; s1[i]; i++)
+		result[i] = s1[i];
+	for (j = 0; s2[j]; j++, i++)
+		result[i] = s2[j];
+	result[i] = '\0';
+	return (result);
+}

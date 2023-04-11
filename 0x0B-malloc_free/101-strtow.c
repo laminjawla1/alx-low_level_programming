2ef47c183836caf784890e335d6f1dbe3c1e595a
Lamin Jawla <laminjawla1001@gmail.com>
2023-04-11 15:17:42 +0000

    strtow

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <ctype.h>
+/**
+*get_n_words - get the number of words in string
+*@s: string
+*Return: The number of words
+*/
+int get_n_words(char *s)
+{
+	int total = 0, i;
+
+	for (i = 0; s[i]; i++)
+		if (s[i] == ' ' || s[i] == '\n')
+			total++;
+	return (total);
+}
+/**
+*strtow - splits all words in a string
+*@str: string
+*Return: Pointer to the split str
+*/
+char **strtow(char *str)
+{
+	char **words;
+	int n_words;
+
+	if (!str)
+		return (NULL);
+	n_words = get_n_words(str);
+	words = (char **)malloc(sizeof(char *) * (n_words + 1));
+	if (!words)
+		return (NULL);
+	return (words);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+char **strtow(char *str);
 char *create_array(unsigned int size, char c);
 void _putchar(int c);
 char *_strdup(char *str);

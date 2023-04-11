c6d9e1b7870dd3492a6d6b11ea98c205a103ecce
Lamin Jawla <laminjawla1001@gmail.com>
2023-04-11 12:31:54 +0000

    alloc grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+/**
+*_memcpy - copy c to all the bytes in a
+*@a: a dynamic 2d array
+*@w: width
+*@h: Height
+*@c: element
+*Return: void
+*/
+void _memcpy(int **a, int w, int h, int c)
+{
+	int i, j;
+
+	for (i = 0; i < h; i++)
+	{
+		for (j = 0; j < w; j++)
+		{
+			a[i][j] = c;
+		}
+	}
+}
+/**
+*alloc_grid - allocate width x height memory to a
+*@width: width
+*@height: Height
+*Return: int **
+*/
+int **alloc_grid(int width, int height)
+{
+	int **a, i;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+	a = (int **)malloc(sizeof(int) * (height));
+	if (!a)
+		return (NULL);
+	for (i = 0; i < width; i++)
+		a[i] = (int *)malloc(sizeof(int) * width);
+
+	_memcpy((int **)a, width, height, 0);
+	return ((int **)a);
+}

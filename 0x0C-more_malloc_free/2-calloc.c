6993bb9883339589339a21a2d64dbe98204b3606
Lamin Jawla <laminjawla1001@gmail.com>
2023-04-13 11:54:12 +0000

    _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,19 @@
 #include <stdlib.h>
 /**
+*_memset - sets the bytes of s to c
+*@s: buffer
+*@c: data
+*@size: size
+*/
+void _memset(void *s, int c, int size)
+{
+	int i;
+	unsigned char *p = s;
+
+	for (i = 0; i < size; i++)
+		*p++ = (unsigned char)c;
+}
+/**
 *_calloc - allocates memory to nmemb of size size
 *@nmemb: members
 *@size: size
@@ -14,5 +28,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	array = malloc(nmemb * size);
 	if (!array)
 		return (NULL);
+	_memset(array, 0, nmemb * size);
 	return (array);
 }

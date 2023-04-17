d1ae13e6993097766bd50e00e083b2e351ea5b1d
Lamin Jawla <laminjawla1001@gmail.com>
2023-04-17 15:32:41 +0000

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+*struct dog - A dog type blue print
+*@name: Name of the dog
+*@age: age of the dog
+*@owner: Owner of the dog
+*
+*Description: Sets a blue print for the dog object
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

61c0a1bb3dc245a189ec743b32e9679f067eb79d
Lamin Jawla <laminjawla1001@gmail.com>
2023-04-17 16:51:08 +0000

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -52,7 +52,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog);
 		return (NULL);
 	}
-	_strcpy(new_dog->name, name);
+	new_dog->name = _strcpy(new_dog->name, name);
 
 	new_dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
 	if (!new_dog->owner)
@@ -61,6 +61,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog->name);
 		return (NULL);
 	}
+	new_dog->owner = _strcpy(new_dog->owner, owner);
 	new_dog->age = age;
 	return (new_dog);
 }

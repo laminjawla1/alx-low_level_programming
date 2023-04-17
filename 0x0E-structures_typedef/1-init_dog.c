8c72b8076b90886678c258018bdddf9eebf9cba5
Lamin Jawla <laminjawla1001@gmail.com>
2023-04-17 15:40:29 +0000

    Dog init

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*init_dog - initialize a dog object
+*@d: Dog obj
+*@name: Name
+*@age: Age
+*@owner: Owner
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}

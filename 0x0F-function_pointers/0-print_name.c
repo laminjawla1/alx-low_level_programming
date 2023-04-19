f7e206d86577fc76c93c0e41c0d0fac3846e5f6f
Lamin Jawla <laminjawla1001@gmail.com>
2023-04-19 12:49:24 +0000

    what's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,9 @@
+/**
+*print_name - prints a name on the screen
+*@name: Name to be printed
+*@f: Pointer to function that takes a char* as arg
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}

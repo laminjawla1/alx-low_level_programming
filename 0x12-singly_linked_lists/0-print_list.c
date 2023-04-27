4127ff140f02ff999d921b221b89678a735014f2
Lamin Jawla <laminjawla1001@gmail.com>
2023-04-27 12:29:11 +0000

    betty error fixed

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,23 +8,13 @@ size_t print_list(const list_t *h)
 {
 	size_t cnt = 0;
 
-	while (h->next)
+	while (h)
 	{
 		if (h->str)
 			printf("[%u] %s\n", h->len, h->str);
 		else
 			printf("[0] %s\n", "(nil)");
 		h = h->next;
-		cnt++;
-	}
-	if (h->str)
-	{
-		printf("[%u] %s\n", h->len, h->str);
-		cnt++;
-	}
-	else
-	{
-		printf("[0] (nil)\n");
 		cnt++;
 	}
 	return (cnt);

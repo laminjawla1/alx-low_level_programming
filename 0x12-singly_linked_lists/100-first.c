f71d7a3a5dc175965c11401ba9fdc8eaf5c5e5d0
Lamin Jawla <laminjawla1001@gmail.com>
2023-04-27 15:53:22 +0000

    betty style fixed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,5 +5,6 @@
 */
 void execute_before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
 }

3bb53ad972e1ac18e4d356c60ee43619d3818354
Lamin Jawla <laminjawla1001@gmail.com>
2023-04-27 17:24:11 +0000

    Hello, Holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,11 @@
+; A program to output "Hello, Holberton"
+
 	global	main
 	extern	printf
 main:
-	mov	rax, 1
-	mov	rdi, 1
-	mov	rsi, fmt
+	mov	edi, fmt
 	xor	eax, eax
 	call	printf
 	mov	eax, 0
 	ret
-fmt:	db	"Hello, Holberton", 10, 0
+fmt: db "Hello, Holberton", 10, 0

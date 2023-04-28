6ababdbdb50d4cd2521aabc77e9015b5c5e9d258
Lamin Jawla <laminjawla1001@gmail.com>
2023-04-28 12:40:34 +0000

    get node at

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+*get_nodeint_at_index - gets a node at a given index
+*@head: Head of the list
+*@index: Index
+*Return: Node if found otherwise NULL
+*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	size_t i;
+
+	for (i = 0; head || i <= index; i++, head = head->next)
+		if (i == index)
+			return (head);
+	return (NULL);
+}

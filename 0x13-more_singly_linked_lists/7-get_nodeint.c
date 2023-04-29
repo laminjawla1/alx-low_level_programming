eb99d5779a0b9981f0f60144a0db77dad491789f
Lamin Jawla <laminjawla1001@gmail.com>
2023-04-29 16:54:17 +0000

    fixed get node at idx

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,7 +10,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	size_t i;
 
-	for (i = 0; head || i <= index; i++, head = head->next)
+	for (i = 0; head && i <= index; i++, head = head->next)
 		if (i == index)
 			return (head);
 	return (NULL);

8fe567ba5350a986d4d640a1426514bf98f3a880
Lamin Jawla <laminjawla1001@gmail.com>
2023-04-29 16:14:46 +0000

    Task5 sets head to NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,9 +12,10 @@ void free_listint2(listint_t **head)
 
 	while (*head)
 	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+		tmp = NULL;
 	}
 	*head = NULL;
 }

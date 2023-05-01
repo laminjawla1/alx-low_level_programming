956f963e07c7b5a3dd1ca65ae9d71e06f7a8d27a
Lamin Jawla <laminjawla1001@gmail.com>
2023-05-01 08:40:51 +0000

    add node at n

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	tmp = *head;
 	for (i = 0; i < idx - 1 && tmp; i++)
 		tmp = tmp->next;
+	if (!tmp)
+		return (NULL);
 	node->next = tmp->next;
 	tmp->next = node;
-	return (*head);
+	return (node);
 }

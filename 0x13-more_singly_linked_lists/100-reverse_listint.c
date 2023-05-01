a4f8e6a9b6bf05a679f4b2454461ee8b5ccca893
Lamin Jawla <laminjawla1001@gmail.com>
2023-05-01 07:40:33 +0000

    delete at_n

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+*reverse_listint - reversed a linked lists
+*
+*@head: Head of the linked list
+*
+*Return: Reversed list
+*/
+listint_t *reverse_listint(listint_t **head)
+{
+	
+}

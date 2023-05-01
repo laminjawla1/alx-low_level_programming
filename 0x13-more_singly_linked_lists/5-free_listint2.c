da608006420ba0175a8f5e3de68a9093f8bb9079
Lamin Jawla <laminjawla1001@gmail.com>
2023-05-01 08:12:17 +0000

    free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,16 +5,16 @@
 */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
+	listint_t *tmp;
 
 	if (!head)
 		return;
 
 	while (*head)
 	{
-		temp = (*head)->next;
+		tmp = (*head)->next;
 		free(*head);
-		*head = temp;
+		*head = tmp;
 	}
 	*head = NULL;
 }

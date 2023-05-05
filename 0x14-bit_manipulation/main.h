5684941d714cb52cf8273a16309f2d18c1a91105
Lamin Jawla <laminjawla1001@gmail.com>
2023-05-05 11:42:32 +0000

    set bit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,10 +2,11 @@
 #define MAIN_H
 
 #include <stdlib.h>
-
+#define BIT_OFFSET 63
 unsigned int binary_to_uint(const char *b);
 int _putchar(int c);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 
 #endif

c6d6aa917f3b17240be8ccf8c4882e865b4b5b42
Lamin Jawla <laminjawla1001@gmail.com>
2023-05-08 19:00:14 +0000

    create file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,6 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <stdlib.h>
-
+int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 #endif

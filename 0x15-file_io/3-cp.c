46f61382c29b457bef0e9f17b76ff29045ca3245
Lamin Jawla <laminjawla1001@gmail.com>
2023-05-09 08:34:28 +0000

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,34 @@
 #include "main.h"
 /**
+*create_buffer - creates a one kilobyte buffer
+*@file: File
+*Return: buffer on success
+*/
+char *create_buffer(char *file)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+	if (!buffer)
+	{
+		dprintf(STDERR_FILENO, "Error: can't write to file %s\n", file);
+		exit(100);
+	}
+	return (buffer);
+}
+/**
+*_close - closes a file
+*@fildes: File Descriptor
+*/
+void _close(int fildes)
+{
+	if (close(fildes))
+	{
+		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", fildes);
+		exit(100);
+	}
+}
+/**
 *main - A program that copies one file content to the other
 *@argc: Aargument count
 *@argv: Argument vector
@@ -7,7 +36,7 @@
 */
 int main(int argc, char *argv[]__attribute__((unused)))
 {
-	int file_from, file_to;
+	int file_from, file_to, bytes;
 	char *buffer;
 
 	if (argc != 3)
@@ -22,21 +51,25 @@ int main(int argc, char *argv[]__attribute__((unused)))
 		exit(98);
 	}
 	file_to = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
-	if (file_to == -1)
+	buffer = create_buffer(argv[2]);
+	while ((bytes = read(file_from, buffer, BUFF_SIZE)) > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: can't write to file %s\n", argv[2]);
-		exit(99);
+		if (bytes == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(99);
+		}
+		bytes = write(file_to, buffer, bytes);
+		if (bytes == -1 || file_to == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
 	}
-	buffer = malloc(sizeof(char) * 1024);
-	if (!buffer)
-	{
-		dprintf(STDERR_FILENO, "Error: can't write to file %s\n", argv[2]);
-		exit(100);
-	}
-	while ((read(file_from, buffer, BUFF_SIZE)) > 0)
-		dprintf(file_to, "%s", buffer);
 	free(buffer);
-	close(file_from);
-	close(file_to);
+	_close(file_from);
+	_close(file_to);
 	return (0);
 }

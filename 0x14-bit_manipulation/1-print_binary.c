76d06e4f5ce691c6f601aa53cc42fd7f2fcb2fe4
Lamin Jawla <laminjawla1001@gmail.com>
2023-07-06 16:42:49 +0000

    Count bits

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,6 +1,21 @@
 #include "main.h"
 
 /**
+* count_bits - Count the number of bits in a number
+*
+*@n: Number
+*
+*Return: Number of bits in a given number
+*/
+int count_bits(unsigned long int n)
+{
+	int number_of_bits;
+
+	for (number_of_bits = 0; n; n >>= 1)
+		number_of_bits++;
+	return (number_of_bits);
+}
+/**
 *print_binary - prints a number in binary
 *
 *@n: Number
@@ -15,11 +30,6 @@ void print_binary(unsigned long int n)
 		_putchar('0');
 		return;
 	}
-	for (i = 63; i >= 0; i--)
-	{
-		if ((n >> i) & 1)
-			_putchar('1');
-		else
-			_putchar('0');
-	}
+	for (i = count_bits(n) - 1; i >= 0; i--)
+		((n >> i) & 1) ? _putchar('1') : _putchar('0');
 }

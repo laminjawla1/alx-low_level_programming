a0ded03f77e6913ca74ebc0b68e28039fb24e66b
Lamin Jawla <laminjawla1001@gmail.com>
2023-07-11 09:25:29 +0000

    close elf file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -10,20 +10,51 @@
 */
 int main(int argc, char *argv[])
 {
+	/*Variable Declaration*/
 	Elf64_Ehdr *elf_header;
 	int sample_elf_file;
 
+	/*Checking for the right number comman-line arguments*/
 	if (argc != 2)
 	{
 		printf("Usage: elf_header elf_filename\n");
 		exit(98);
 	}
 
+	/*Open the file*/
 	sample_elf_file = open(argv[1], O_RDONLY);
+	/*Checking if opening was successful*/
 	if (sample_elf_file == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file <%s>\n", argv[1]);
 		exit(98);
 	}
+	/*Allocate sum chunk of bytes for the elf_header*/
+	elf_header = malloc(sizeof(Elf64_Ehdr));
+	/*Check if the allocation was successful*/
+	if (!elf_header)
+	{
+		_close(sample_elf_file);
+		dprintf(STDERR_FILENO, "Error: Insufficient memory\n");
+		exit(98);
+	}
 	return (0);
+}
+/**
+* _close - Closes an elf file
+*
+*@sample_elf_file: File to closed
+*
+*Return: None
+*/
+void _close(int sample_elf_file)
+{
+	/*Try closing the file, if an error is enxountered, exit with code <98>*/
+	if ((close(sample_elf_file)) == -1)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Unable to close the file with a descriptor of <%d>\n",
+			sample_elf_file);
+		exit(98);
+	}
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -19,5 +19,6 @@
 int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 int append_text_to_file(const char *filename, char *text_content);
+void _close(int sample_elf_file);
 
 #endif

e41d46983b807f0709fe0835a9781e41f501752e
Lamin Jawla <laminjawla1001@gmail.com>
2023-08-17 15:00:36 +0000

    Insert at

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -36,7 +36,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (!node)
 		return (NULL);
-	if (idx > len(*h))
+	if (idx > len(*h) - 1)
 		return (NULL);
 	if (idx == 0)
 	{

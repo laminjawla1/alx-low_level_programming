dfd93a1db716db5d0129e8009cda510d189da241
Lamin Jawla <laminjawla1001@gmail.com>
2023-08-21 10:13:51 +0000

    Let's call C functions from Python

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,60 @@
+/**
+* add - Adds two integers
+*
+*@a: Operand one
+*@b: Operand two
+*
+*Return: a + b
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+* sub - Subtract two integers
+*
+*@a: Operand one
+*@b: Operand two
+*
+*Return: a - b
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+* mul - Multiplies two integers
+*
+*@a: Operand one
+*@b: Operand two
+*
+*Return: a * b
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+* div - Divides two integers
+*
+*@a: Operand one
+*@b: Operand two
+*
+*Return: a / b
+*/
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+* mod - gets the mod of two integers
+*
+*@a: Operand one
+*@b: Operand two
+*
+*Return: a % b
+*/
+int mod(int a, int b)
+{
+	return (a % b);
+}

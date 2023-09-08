21591d9752a5c78cdaf5553399a19c36eee34728
Lamin Jawla <laminjawla1001@gmail.com>
2023-09-08 10:49:34 +0000

    hash table set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,50 @@
 #include "hash_tables.h"
 
+/**
+* create_node - Creates a new node
+*
+*@k: Key
+*@v: Value
+*
+*Return: A new node else NULL
+*/
+hash_node_t *create_node(const char *k, const char *v)
+{
+	hash_node_t *node = malloc(sizeof(hash_node_t));
+
+	if (!node)
+		return (NULL);
+	node->key = strdup(k);
+	node->value = strdup(v);
+	node->next = NULL;
+	return (node);
+}
+/**
+* hash_table_set - Inserts a new element into the table
+*
+*@ht: Hash Table
+*@key: Key
+*@value: Value
+*
+*Return: 1 on sucess else 0
+*/
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	unsigned long int index;
+	hash_node_t *node;
 
+	if (!key || !ht || value == NULL)
+		return (0);
+	node = create_node(key, value);
+	if (!node)
+		return (0);
+	index = key_index((const unsigned char *) key, ht->size);
+	if (ht->array[index])
+	{
+		node->next = ht->array[index];
+		ht->array[index] = node;
+	}
+	else
+		ht->array[index] = node;
+	return (1);
 }

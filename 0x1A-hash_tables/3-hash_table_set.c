d6372f10e7af7946b4c79f888b4f2c762570e1c8
Lamin Jawla <laminjawla1001@gmail.com>
2023-09-08 11:04:08 +0000

    hash table set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -43,10 +43,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!cpy)
 		return (0);
 	index = key_index((const unsigned char *) key, ht->size);
-	if (ht->array[index] != NULL)
+	if (ht->array[index])
 	{
-		free(ht->array[index]->value);
-		ht->array[index]->value = cpy;
+		node->next = ht->array[index];
+		ht->array[index] = node;
 	}
 	else
 		ht->array[index] = node;

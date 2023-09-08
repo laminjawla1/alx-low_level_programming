d77c4a2aa9058a2bd67db99bfc5e6088f7a4f6f2
Lamin Jawla <laminjawla1001@gmail.com>
2023-09-08 08:21:53 +0000

    hash

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,6 +10,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	unsigned long int i;
 
 	table = malloc(sizeof(hash_table_t));
 	if (!table)
@@ -18,5 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->array = (hash_node_t **)malloc(sizeof(hash_node_t) * size);
 	if (!table->array)
 		return (NULL);
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
 	return (table);
 }

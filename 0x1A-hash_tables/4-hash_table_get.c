1a53c67554833c3414ddbc333feb874e11493143
Lamin Jawla <laminjawla1001@gmail.com>
2023-09-08 11:42:58 +0000

    hash table get

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_get - Gets an element from the hash table
+*
+*@ht: Hash Table
+*@key: Key of the value
+*
+*Return: A value else NULL
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index, i;
+
+	index = key_index((const unsigned char *) key, ht->size);
+
+	for (i = index; ht->array[i]; i++)
+		if (strcmp(ht->array[i]->key, key) == 0)
+			return (ht->array[i]->value);
+	return (NULL);
+}

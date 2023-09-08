cbaceb63d7ce8fc0ebd86225e0f5e40c785ccee8
Lamin Jawla <laminjawla1001@gmail.com>
2023-09-08 12:18:55 +0000

    hash table get

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,13 +10,18 @@
 */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index, i;
+	unsigned long int index;
+	hash_node_t *cursor;
 
+	index = key_index((const unsigned char *) key, ht->size);
 	if (!ht)
 		return (NULL);
-	index = key_index((const unsigned char *) key, ht->size);
-	for (i = index; ht->array[i]; i++)
-		if (strcmp(ht->array[i]->key, key) == 0)
-			return (ht->array[i]->value);
+	cursor = ht->array[index];
+	while (cursor)
+	{
+		if (strcmp(cursor->key, key) == 0)
+			return (cursor->value);
+		cursor = cursor->next;
+	}
 	return (NULL);
 }

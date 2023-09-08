724c2edf5fe1231706595a8bf848d5a8b2a7f687
Lamin Jawla <laminjawla1001@gmail.com>
2023-09-08 08:38:39 +0000

    0. >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,11 +13,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 
 	table = malloc(sizeof(hash_table_t));
-	if (!table)
+	if (table == NULL)
 		return (NULL);
 	table->size = size;
 	table->array = malloc(sizeof(hash_node_t *) * size);
-	if (!table->array)
+	if (table->array == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		table->array[i] = NULL;

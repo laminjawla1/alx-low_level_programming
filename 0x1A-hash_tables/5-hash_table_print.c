b5ad8b918d7ce4fcbb9077aa311d6705dc655722
Lamin Jawla <laminjawla1001@gmail.com>
2023-09-08 16:29:48 +0000

    hash table get

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_print - Prints the hash table
+*
+*@ht: The hash table
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *cursor;
+	unsigned long int i;
+	int flag = 1;
+
+	if (ht == NULL)
+	{
+		printf("{}\n");
+		return;
+	}
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		cursor = ht->array[i];
+		while (cursor)
+		{
+			if (!flag)
+				printf(", ");
+			printf("'%s': '%s'", cursor->key, cursor->value);
+			flag = 0;
+			cursor = cursor->next;
+		}
+	}
+	printf("}\n");
+}

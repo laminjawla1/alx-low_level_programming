0bc30c84eac36f32b22f0ee266e93e169d96ab4b
Lamin Jawla <laminjawla1001@gmail.com>
2024-01-16 06:28:09 -0800

    Onterpolation search

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,38 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_search - Searches for a value in an array
+ * using the interpolation search algorithm
+ *
+ * @array: The array to search in
+ * @size: The size of the array
+ * @value: The value to look for
+ *
+ * Return: The index of the value or -1
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t low, high, pos;
+
+	if (array == NULL)
+		return (-1);
+
+	low = 0;
+	high = size - 1;
+
+	while (value >= array[low] && value <= array[high])
+	{
+		pos = low + (((double)(high - low) / (array[high] - array[low])) *
+				(value - array[low]));
+
+		printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
+		if (array[pos] == value)
+			return (pos);
+		else if (array[pos] > value)
+			high = pos;
+		else
+			low = pos + 1;
+	}
+	return (-1);
+
+}

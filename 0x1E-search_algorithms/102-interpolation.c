a6b95ec9c3cf691c391f7ab77cd70f16b863e99a
Lamin Jawla <laminjawla1001@gmail.com>
2024-01-16 06:42:24 -0800

    Interpolatio search

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -20,12 +20,20 @@ int interpolation_search(int *array, size_t size, int value)
 	low = 0;
 	high = size - 1;
 
-	while (value >= array[low] && value <= array[high] && low <= high)
+	/*while (value >= array[low] && value <= array[high] && low <= high)*/
+	while (1)
 	{
 		pos = low + (((double)(high - low) / (array[high] - array[low])) *
 				(value - array[low]));
 
+		if (pos >= size)
+		{
+			printf("Value checked array[%ld] is out of range\n", pos);
+			break;
+		}
+
 		printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
+
 		if (array[pos] == value)
 			return (pos);
 		else if (array[pos] > value)

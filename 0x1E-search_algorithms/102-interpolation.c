bb39be3a01ba45a3fe44f545a76a4d8b2d55ca9e
Lamin Jawla <laminjawla1001@gmail.com>
2024-01-16 06:29:49 -0800

    Onterpolation search

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -20,7 +20,7 @@ int interpolation_search(int *array, size_t size, int value)
 	low = 0;
 	high = size - 1;
 
-	while (value >= array[low] && value <= array[high])
+	while (value >= array[low] && value <= array[high] && low <= high)
 	{
 		pos = low + (((double)(high - low) / (array[high] - array[low])) *
 				(value - array[low]));
